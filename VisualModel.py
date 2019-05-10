9eebd6116781d0db36251a1c200d118472286e13
Omar Ali Sheikh-Omar <sheikhomar@users.noreply.github.com>
2019-05-10 12:26:54 +0200

    Attempt to comptue bounding boxes.

--- VisualModel.py
+++ VisualModel.py
@@ -192,6 +192,7 @@ class VisModel():
 
         for l in model.layers:
             if isinstance(l, Conv2D):
+
                 self.layers.append(VisConv2D(l))
                 self.layers.append(VisActivation(l))
             elif isinstance(l, MaxPooling2D):
@@ -254,3 +255,114 @@ class VisModel():
             self.layers[j].down(self.layers[j + 1].down_data)
 
         return self.layers[0].down_data
+
+    def down_bound(self, up_data):
+        top_features = list(self.get_top_features(up_data, n=1, max_only=True))
+        rank, top_feature_idx, feature_maps = top_features[0]
+        top_feature_map = feature_maps[:, :, :, top_feature_idx]
+
+        prev_layer = self.layers[-2].layer
+
+        # Compute the filter size and padding
+        filter_size, stride, padding = self.get_filter_params(prev_layer)
+        batch, width, height = top_feature_map.shape
+        highest_act_index = top_feature_map.argmax()
+
+        print('  Width: {} Height: {}, Filter'.format(width, height))
+
+        bbox = self.compute_bounding_box(
+            width=width,
+            height=height,
+            filter_size=filter_size,
+            padding=padding,
+            index=highest_act_index
+        )
+        print('Found bounding box: {}'.format(bbox))
+
+        start_index = bbox['start_index']
+        end_index = bbox['end_index']
+
+        for i in range(len(self.layers)-3, 0, -1):
+            vis_layer = self.layers[i]
+            if isinstance(vis_layer, VisConv2D) or isinstance(vis_layer, VisMaxPooling2D):
+                layer = vis_layer.layer
+                print('Processing: {}'.format(layer.name, i))
+                filter_size, stride, padding = self.get_filter_params(layer)
+                batch, width, height, channels = layer.output_shape
+                bbox_start = self.compute_bounding_box(
+                    width=width,
+                    height=height,
+                    filter_size=filter_size,
+                    padding=padding,
+                    index=start_index
+                )
+
+                print(' Start index: {}'.format(bbox_start))
+                print('  Width: {} Height: {}'.format(width, height))
+
+                bbox_end = self.compute_bounding_box(
+                    width=width,
+                    height=height,
+                    filter_size=filter_size,
+                    padding=padding,
+                    index=end_index
+                )
+
+                print(' End index:   {}'.format(bbox_end))
+                start_index = bbox_start['start_index']
+                end_index = bbox_end['end_index']
+
+            elif isinstance(vis_layer, VisInput):
+                print('Reach the input layer: {}'.format((start_index, end_index)))
+
+        return bbox
+
+    def get_filter_params(self, layer):
+        conf = layer.get_config()
+        if 'kernel_size' in conf:
+            pool_size = conf['kernel_size']
+        elif 'pool_size' in conf:
+            pool_size = conf['pool_size']
+        else:
+            raise Exception('Unknown layer: {}'.format(conf))
+        assert (pool_size[0] == pool_size[1])
+        pool_size = pool_size[0]
+
+        stride = conf['strides']
+        assert (stride[0] == stride[1])
+        stride = stride[0]
+
+        padding = conf['padding']
+        if padding == 'same':
+            padding = int((pool_size - 1) / 2)
+        elif padding == 'valid':
+            padding = 0
+        else:
+            raise Exception('Unknown padding type: {}'.format(padding))
+        return pool_size, stride, padding
+
+    def compute_bounding_box(self, width, height, filter_size, padding, index):
+        row_idx = int(np.ceil((index + 1) / width))
+        column_idx = index % height
+
+        rf_row_start_idx = row_idx
+        rf_column_start_idx = column_idx
+        rf_row_end_idx = rf_row_start_idx + filter_size - 1
+        rf_column_end_idx = rf_column_start_idx + filter_size - 1
+
+        rf_row_start_idx = max(0, rf_row_start_idx - padding)
+        rf_column_start_idx = max(0, rf_column_start_idx - padding)
+        rf_row_end_idx = min(width - 1, rf_row_end_idx - padding)
+        rf_column_end_idx = min(height - 1, rf_column_end_idx - padding)
+
+        start_index = width*(rf_row_start_idx + 1) - (width - rf_column_start_idx)
+        end_index = width * (rf_row_end_idx + 1) - (width - rf_column_end_idx)
+
+        return {
+            'start_index': start_index,
+            'end_index': end_index,
+            'start_row': rf_row_start_idx,
+            'start_column': rf_column_start_idx,
+            'end_row': rf_row_end_idx,
+            'end_column': rf_column_end_idx
+        }
